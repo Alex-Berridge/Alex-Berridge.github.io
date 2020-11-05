6c9ac9f122e2229f47adcdbea412d7a62a4c3704
ClientAd-HonourShelgon408 <44262132+HonourShelgon408@users.noreply.github.com>
2020-11-05 11:32:05 +0000

    Minor changes
    added mobilecheck function - likely not to be used since i will take a mobile-first approach
    Formatting and terminology in sw.js made consistent
    Changes to manifest colours and names

--- sw.js
+++ sw.js
@@ -7,12 +7,12 @@ var filesToCache = [ /* array of filenames referenced by relativity */
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
+self.addEventListener('install', function(event) {
+  event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache).then(function(){
         console.log("installed");
-      }); //need to handle addall since if one fails they all fail
+      }); //need to handle addAll since if one fails they all fail
     })
   );
 });
@@ -21,15 +21,12 @@ self.addEventListener('activate', function(){
   console.log('now ready to handle fetches!');
 });
 
-
 self.addEventListener('fetch', function(event) {
   console.log("fetched ", event.request.url);
- });
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      console.log("fetched ", event.request.url);
+      return response || fetch(event.request);
     })
   );
 });
